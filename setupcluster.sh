c52f9ceddffa22a855574f52e77bfc5e17c3f7b0
tt5 <2585803+tt5@users.noreply.github.com>
2022-06-27 13:25:19 +0000

    setup knative

--- setupcluster.sh
+++ setupcluster.sh
@@ -3,5 +3,3 @@ wget https://github.com/knative/client/releases/download/knative-v1.5.0/kn-linux
 mv kn-linux-amd64 kn
 chmod +x kn
 sudo mv kn /usr/local/bin
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.5.0/serving-crds.yaml
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.5.0/serving-core.yaml
--- setupistio.sh
+++ setupistio.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+curl -L https://istio.io/downloadIstio
+export PATH="$PATH:/workspace/gitpod-test-1/istio-1.14.1/bin"
+istioctl install --set profile=demo -y
+$ export EXTERNAL_IP=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
